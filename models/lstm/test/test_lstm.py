e92bd85b1faff766c94fdd68c5ad8bc5c3d4c5b5
Sean M. Gonzalez <sean.moore.gonzalez@gmail.com>
2015-06-12 12:23:31 -0400

    finished first pass at translating Core for LSTM ingestion

---
+++ models/lstm/test/test_lstm.py
@@ -0,0 +1,8 @@
+"""
+This is designed to ensure the synapsify_preprocess.py produces the same output as
+the imdb_preprocess.py.
+"""
+
+dataset_path = "../data/test/"
+# Would like to add a manifest one day, for now we'll use all the files
+
