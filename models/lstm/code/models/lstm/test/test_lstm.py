afcaf687c98680edddca2a4886ac9972ac796d29
Ruofan Kong <krf1088@gmail.com>
2015-06-18 12:06:30 -0400

    training data removed, scode files tracked

--- models/lstm/code/models/lstm/test/test_lstm.py
+++
@@ -1,8 +0,0 @@
-"""
-This is designed to ensure the synapsify_preprocess.py produces the same output as
-the imdb_preprocess.py.
-"""
-
-dataset_path = "../data/test/"
-# Would like to add a manifest one day, for now we'll use all the files
-
